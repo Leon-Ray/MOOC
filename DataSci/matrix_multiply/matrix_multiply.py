5e5e01a9bf484b78383663e00b26f4c52e793fc5
unknown <lraykin@pc18099.iteris.com>
2014-07-21 20:00:48 -0700

    minor edit

--- DataSci/matrix_multiply/matrix_multiply.py
+++ DataSci/matrix_multiply/matrix_multiply.py
@@ -11,13 +11,16 @@ mr = MapReduce.MapReduce()
 # Do not modify above this line
 
 def mapper(record):
-    
+
+    #as recommended in the course, dimensions are hardcoded 
+    #(two passes can't be done in the toy example)
     k = [0,1,2,3,4]
 
     if record[0] == "a":
         for N  in k:
             key = tuple((record[1], N))
             value = record
+            print key, value
             mr.emit_intermediate(key, value)
             
     if record[0] == "b":
@@ -26,6 +29,7 @@ def mapper(record):
                 if record[2] == L:
                     key = tuple((N, L))
                     value = record
+                    print key, value
                     mr.emit_intermediate(key, value)
                     
 
@@ -34,6 +38,7 @@ def reducer(key, list_of_values):
     total = 0
     for i in list_of_values:
         for j in list_of_values:
+            print i, j
             if i[0] == "a" and j[0] == "b":
                 if i[2] == j[1]:
                     total+= i[3]*j[3]
--- Matrix/README.md
+++ Matrix/README.md
@@ -5,7 +5,7 @@ Purpose
 -------
 Assignments from the Coursera course *Coding the Matrix: Linear Algebra for Computer Science Applications.*
 
-The assignments involved writing functions that perform basic linear algebra operations on objects of class *vector* and *matrix*, which were created for the course.
+The assignments involved writing functions that perform basic linear algebra operations in methods of class *vector* and *matrix*, which were created for the course.
 
 Python Version 
 --------------
